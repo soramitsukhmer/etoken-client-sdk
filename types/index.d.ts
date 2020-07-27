357c8f8fc7db5683783c8c35d1cd46807d284216
Socheat Sok <alex@socheat.net>
2020-07-27 12:12:46 +0700

    feat(type): add typescript type declaration

---
+++ types/index.d.ts
@@ -0,0 +1,66 @@
+import { Bus } from "../src/core/Emittable";
+
+type TokenResponseType = {
+  certchain: string,
+  signature: string,
+  signeddata: string,
+}
+
+export class Emittable {
+  /**
+   * Set Listener
+   * @param listener
+   */
+  setListener(listener: Bus): void;
+
+  /**
+   * @param topic
+   * @param payload
+   */
+  emit(topic: string, payload: any): () => void;
+
+  /**
+   * @param topic
+   * @param payload
+   */
+  publish(topic: string, payload: any): () => void;
+
+  /**
+   * @param topic
+   * @param callback
+   */
+  on(topic: string, callback: () => {}): () => void;
+
+  /**
+   * @param topic
+   * @param callback
+   */
+  subscribe(topic: string, callback: () => {}): () => void;
+}
+
+export class SafeNet extends Emittable {
+  /**
+   * Request eToken Prompt
+   * @param scheme URL Scheme
+   * @param target Open Target
+   */
+  request(scheme: string = 'token', target: string = '_self'): void;
+
+  /**
+   * Sign data using eToken
+   * @param data
+   */
+  sign(data: any): Promise<TokenResponseType>;
+
+  /**
+   * Terminate the connection
+   * @param error
+   */
+  terminate(error: string): void;
+
+  /**
+   * Create a SHA256 Hash
+   * @param data
+   */
+  static hash(data: any): string
+}
